a7663d1fedf162f124ae3c526b994f76237b4af5
Julian Friedman <julz.friedman@uk.ibm.com>
2015-04-27 09:43:31 +0100

    more wip - make mounting proc and tmp work, get exit statuses back, start on waitpid to remove zombies

--- old/linux_backend/skeleton/lib/hook-parent-before-clone.sh
+++ old/linux_backend/skeleton/lib/hook-parent-before-clone.sh
@@ -14,6 +14,10 @@ cp bin/wshd $rootfs_path/sbin/wshd
 chown $root_uid:$root_uid $rootfs_path/sbin/wshd
 chmod 700 $rootfs_path/sbin/wshd
 
-mkdir -p $rootfs_path/dev/pts
-mount -n -t devpts -o newinstance,ptmxmode=0666 devpts $rootfs_path/dev/pts
-mkdir -p $rootfs_path/dev/shm
+# mkdir -p $rootfs_path/dev/pts
+# mount -n -t devpts -o newinstance,ptmxmode=0666 devpts $rootfs_path/dev/pts
+# mkdir -p $rootfs_path/dev/shm
+
+# hack to ensure proc is owned by correct user until translation
+mkdir -p $rootfs_path/proc
+chown $root_uid:$root_uid $rootfs_path/proc
